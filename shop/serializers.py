e903be8f37da7d8068afc08c17287ed361342427
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-27 02:04:26 +0430

    shop serializers added

---
+++ shop/serializers.py
@@ -0,0 +1,29 @@
+from . import models
+from rest_framework import serializers
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Order
+        fields = "__all__"
+        read_only_fields = 'created_date'
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Product
+        fields = "__all__"
+        read_only_fields = 'count_seen'
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = "__all__"
+
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.SubCategory
+        fields = "__all__"
+
