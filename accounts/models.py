b31534eeda8bf37ecc8fd1a430ea192ff30dd345
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-27 02:03:39 +0430

    accounts models added

---
+++ accounts/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from phone_field import PhoneField
+
+
+# Create your models here.
+
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True)
+    role_choices = ((1, "User"), (2, "Seller"))
+    role = models.IntegerField(choices=role_choices, default=1)
+    credit = models.IntegerField(blank=False, default=0)
+
+
+class Phone(models.Model):
+    phone = PhoneField(
+        unique=True,
+        error_messages={
+            'unique': "A user with this phone already exists."
+        }
+    )
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Address(models.Model):
+    about = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
