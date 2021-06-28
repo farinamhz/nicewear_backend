6d2846c8289b9e0e47facc5ba19bc80999e7584c
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-06-28 20:17:47 +0430

    create and delete product views added

--- shop/views.py
+++ shop/views.py
@@ -48,3 +48,49 @@ class GetCategoryById(generics.ListAPIView):
         return Response(serializer.data)
 
 
+class CreateProduct(generics.CreateAPIView):
+    serializer_class = serializers.ProductSerializer
+    queryset = models.Product.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        context1 = {
+            'name': request.data["post"],
+            'color1': request.data["color1"],
+            'color2': request.data["color2"],
+            'color3': request.data["color3"],
+            'price': request.data["price"],
+            'seller': request.data["seller"],
+            'picture': request.data["picture"]
+        }
+        self.context2 = {
+            'category1': request.data["category1"],
+        }
+        self.context3 = {
+            'category1': request.data["category1"],
+        }
+
+        serializer = self.get_serializer(data=context1)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+    def perform_create(self, serializer):
+        obj = serializer.save()
+        self.context2['product'] = obj.id
+        self.context3['product'] = obj.id
+        cp_serializer1 = serializers.CategoryProductSerializer(data=self.context2)
+        cp_serializer2 = serializers.CategoryProductSerializer(data=self.context3)
+        if cp_serializer1.is_valid() and cp_serializer2.is_valid():
+            cp_serializer1.save()
+            cp_serializer2.save()
+        else:
+            return Response(cp_serializer1.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteProduct(generics.DestroyAPIView):
+    permission_classes = [IsAuthenticated, ]    # + IsOwner
+    queryset = models.Product.objects.all()
+
+    def get_queryset(self):
+        return models.Product.objects.filter(pk=self.kwargs['pk'], seller=self.request.user)
