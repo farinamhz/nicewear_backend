4a1fbc0542c815b6e4cf007cf166f3ecde525140
Farinam HemmatiZadeh <45711614+farinamhz@users.noreply.github.com>
2021-07-02 21:07:52 +0430

    corsheader added to settings

--- nicewear_backend/settings.py
+++ nicewear_backend/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'accounts',
-    'shop'
+    'shop',
+    'corsheaders'
 
 ]
 
@@ -52,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'nicewear_backend.urls'
